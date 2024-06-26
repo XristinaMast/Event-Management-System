4428891c5ba44901b51809a564be6a8d9f89dfea
XristinaMast <112976903+XristinaMast@users.noreply.github.com>
2024-06-26 20:57:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,56 +9,60 @@ import time
 app.config["MONGO_URI"] = "mongodb://mongodb:27017/HospitalDB"
 mongo = PyMongo(app)
 
-# Connect to our local MongoDB
 client = MongoClient('mongodb://localhost:27017/')
-# Choose database
 db = client['HospitalDB']
-# Choose collections
 doctors = db['Doctors']
 patients = db['Patients']
 appointments = db['Appointments']
+#Γίνεται σύνδεση στη βάση δεδομένων MongoDB και δημιουργούνται οι συλλογές Doctors, Patients και Appointments.
 
-# Initiate Flask App
-app = Flask(__name__)
+app = Flask(__name__) #Εδώ δημιουργείται η εφαρμογή Flask.
 
 def has_required_fields(field_names, data):
+    # Σε αυτήν την συνάρτηση βάζουμε σε list τα ονόματα των fields που πρέπει να περιέχει το json που θα δώσει ο χρήστης.
+    # Αν δεν υπάρχει κάποιο από αυτά, επιστρέφεται False
     for field in field_names:
         if field not in data:
             return False
     return True
 
 users_sessions = list()
-def create_session(username, category):
+def create_session(username, category): # Η συνάρτηση αυτή δημιουργεί και επιστρέφει ένα μοναδικό κωδικό uuid
     user_uuid = str(uuid.uuid1())
     users_sessions.append((user_uuid, username, time.time(), category))
     return user_uuid
 
 def is_session_valid(user_uuid, category):
+    # Η συνάρτηση αυτή ελέγχει αν το uuid είναι έγκυρο και αν ο χρήστης που του ανήκει, έχει πρόσβαση στο category
+    # Είναι έτσι σχεδιασμένο που αν ο χρήστης είναι admin μπορεί να έχει προσβάση στα endpoint που είναι Simple ενώ το αντίθετο δεν ισχύει
     for session in users_sessions:
         if (session[0] == user_uuid) and (session[3] == category or session[3] == "Admin"):
             return True
     return False
+#Αυτές οι συναρτήσεις βοηθούν στον έλεγχο αν υπάρχουν όλα τα απαραίτητα πεδία, στη δημιουργία συνεδριών χρηστών και στον έλεγχο εγκυρότητας συνεδριών.
 
 @app.route('/login', methods=['POST'])
-def login():
+def login(): #Αυτή η διαδρομή χειρίζεται το login των χρηστών, δημιουργεί μια συνεδρία και επιστρέφει ένα UUID μαζί με την κατηγορία του χρήστη.
+    # Το endpoint αυτό χρησιμοποιείται για να συνδεθεί ο χρήστης
     try:
         data = json.loads(request.data)
     except Exception:
         return Response("This request needs JSON data", status=500, mimetype='application/json')
     if not has_required_fields(("username", "password"), data):
         return Response("There is missing at least one field in your request", status=500, mimetype="application/json")
-
+    # Έλεγχος αν υπάρχει κάποιος χρήστης με αυτό το email και τον κωδικό
     user = doctors.find_one({ "$and": [{"username":data['username']},{"password":data['password']}]})
-    if user is None:
+    if user is None:  # Επιτυχής ταυτοποίηση. Επιστροφή uuid
         user = patients.find_one({ "$and": [{"username":data['username']},{"password":data['password']}]})
-    if user is None:
+        user_uuid = create_session(data['username'], user['category'])
+        return Response(json.dumps({"uuid": user_uuid, "category": user['category']}), mimetype='application/json')
+    if user is None:  # Η αυθεντικοποίηση είναι ανεπιτυχής. Μήνυμα λάθους (Λάθος email ή password)
         return Response("Invalid username or password", status=401, mimetype="application/json")
-
-    user_uuid = create_session(data['username'], user['category'])
-    return Response(json.dumps({"uuid": user_uuid, "category": user['category']}), mimetype='application/json')
-
+    
 @app.route('/api/v1/doctors', methods=['POST'])
-def add_doctor():
+def add_doctor(): #Προσθέτουμε έναν νέο γιατρό στη βάση δεδομένων.
+    # Αν υπάρχει ήδη, επιστρέφουμε κατάλληλο μήνυμα
+    # Αν δεν υπάρχει, τον προσθέτουμε και επιστρέφουμε το νέο χρήστη σε μορφή json
     if not has_required_fields(['name', 'specialization', 'username', 'password'], request.json):
         return Response(json.dumps({'error': 'Missing required fields'}), mimetype='application/json')
     try:
@@ -68,7 +72,9 @@ def add_doctor():
         return Response(json.dumps({'error': 'Doctor already exists'}), mimetype='application/json')
 
 @app.route('/api/v1/patients', methods=['POST'])
-def add_patient():
+def add_patient(): #Προσθέτουμε έναν νέο ασθενή στη βάση δεδομένων.
+    # Αν υπάρχει ήδη, επιστρέφουμε κατάλληλο μήνυμα
+    # Αν δεν υπάρχει, τον προσθέτουμε και επιστρέφουμε το νέο χρήστη σε μορφή json
     if not has_required_fields(['name', 'dob', 'username', 'password'], request.json):
         return Response(json.dumps({'error': 'Missing required fields'}), mimetype='application/json')
     try:
@@ -78,7 +84,9 @@ def add_patient():
         return Response(json.dumps({'error': 'Patient already exists'}), mimetype='application/json')
 
 @app.route('/api/v1/appointments', methods=['POST'])
-def add_appointment():
+def add_appointment(): #Προσθέτουμε ένα νέο ραντεβού στη βάση δεδομένων.
+    # Αν υπάρχει ήδη, επιστρέφουμε κατάλληλο μήνυμα
+    # Αν δεν υπάρχει, το προσθέτουμε και επιστρέφουμε το νέο ραντεβού σε μορφή json
     if not has_required_fields(['doctor_id', 'patient_id', 'date'], request.json):
         return Response(json.dumps({'error': 'Missing required fields'}), mimetype='application/json')
     doctor = doctors.find_one({'_id': ObjectId(request.json['doctor_id'])})
@@ -89,7 +97,9 @@ def add_appointment():
     return Response(json.dumps(request.json), mimetype='application/json')
 
 @app.route('/api/v1/doctors/<doctor_id>', methods=['GET'])
-def get_doctor(doctor_id):
+def get_doctor(doctor_id): #Αυτό το κομμάτι κώδικα επιστρέφει πληροφορίες για έναν συγκεκριμένο γιατρό.
+    # Αν δεν υπάρχει, επιστρέφουμε κατάλληλο μήνυμα
+    # Αν υπάρχει, το επιστρέφουμε σε μορφή json
     doctor = doctors.find_one({'_id': ObjectId(doctor_id)}, {'_id': False})
     if doctor:
         return Response(json.dumps(doctor), mimetype='application/json')
@@ -97,7 +107,9 @@ def get_doctor(doctor_id):
         return Response(json.dumps({'error': 'Doctor not found'}), mimetype='application/json')
 
 @app.route('/api/v1/patients/<patient_id>', methods=['GET'])
-def get_patient(patient_id):
+def get_patient(patient_id): #Αυτό το κομμάτι κώδικα επιστρέφει πληροφορίες για έναν συγκεκριμένο ασθενή.
+    # Αν δεν υπάρχει, επιστρέφουμε κατάλληλο μήνυμα
+    # Αν υπάρχει, το επιστρέφουμε σε μορφή json
     patient = patients.find_one({'_id': ObjectId(patient_id)}, {'_id': False})
     if patient:
         return Response(json.dumps(patient), mimetype='application/json')
@@ -105,7 +117,9 @@ def get_patient(patient_id):
         return Response(json.dumps({'error': 'Patient not found'}), mimetype='application/json')
 
 @app.route('/api/v1/appointments/<appointment_id>', methods=['GET'])
-def get_appointment(appointment_id):
+def get_appointment(appointment_id): #Αυτό το κομμάτι κώδικα διαγράφει ένα συγκεκριμένο ραντεβού.
+    # Αν δεν υπάρχει, επιστρέφουμε κατάλληλο μήνυμα
+    # Αν υπάρχει, το επιστρέφουμε σε μορφή json
     appointment = appointments.find_one({'_id': ObjectId(appointment_id)}, {'_id': False})
     if appointment:
         return Response(json.dumps(appointment), mimetype='application/json')
@@ -113,7 +127,9 @@ def get_appointment(appointment_id):
         return Response(json.dumps({'error': 'Appointment not found'}), mimetype='application/json')
 
 @app.route('/api/v1/appointments/<appointment_id>', methods=['DELETE'])
-def delete_appointment(appointment_id):
+def delete_appointment(appointment_id): ##Αυτό το κομμάτι κώδικα επιστρέφει πληροφορίες για έναν συγκεκριμένο γιατρό.
+    # Αν δεν υπάρχει, επιστρέφουμε κατάλληλο μήνυμα
+    # Αν υπάρχει, το επιστρέφουμε σε μορφή json
     appointment = appointments.find_one({'_id': ObjectId(appointment_id)})
     if appointment:
         appointments.delete_one({'_id': ObjectId(appointment_id)})
